2c377980f398fd77257896ac7617b1bdc7545fb7
Joe Thorpe <joe.t@interruptlabs.co.uk>
2023-02-07 21:23:46 +0000

    remove bell in macos when using emacskeys

---
+++ remove-bell-macos-emacskeys.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# https://stackoverflow.com/a/57651703
+
+if [ "$(uname)"  == "Darwin" ]
+then
+    if [ ! -d "/Applications/Visual Studio Code.app" ]
+    then
+        echo -e "[!] vscode not found"
+        exit
+    fi
+    if [ ! -d "${HOME}/Library/KeyBindings/" ]
+    then
+        mkdir -p "${HOME}/Library/KeyBindings/"
+    fi
+    if [ ! -f "${HOME}/Library/KeyBindings/DefaultKeyBinding.dict" ]
+    then
+cat <<EOF > "${HOME}/Library/KeyBindings/DefaultKeyBinding.dict"
+{
+    "@^\UF701" = "noop:";
+    "@^\UF702" = "noop:";
+    "@^\UF703" = "noop:";
+}
+EOF
+    fi
+fi
